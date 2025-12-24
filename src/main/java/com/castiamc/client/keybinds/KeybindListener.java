c67c9873eb3d6feda544280b199fe91a967c506b
Hexon <snicks.phyla0y@icloud.com>
2025-12-24 18:54:30 -0500

    Add KeybindList class for managing keybinds

---
+++ src/main/java/com/castiamc/client/keybinds/KeybindListener.java
@@ -0,0 +1,13 @@
+package com.castiamc.client.keybinds;
+
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+public class KeybindList {
+    private final Map<String, Integer> keybinds = new HashMap<>();
+
+    public KeybindList(Path file) {
+        // placeholder constructor
+    }
+}
