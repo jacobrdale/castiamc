8fc3b475d6f2dc9bbbc0d9f2abe0c8ffe92cc354
Hexon <snicks.phyla0y@icloud.com>
2025-12-24 18:54:52 -0500

    Add KeybindProcessor class with placeholder constructor

---
+++ src/main/java/com/castiamc/client/keybinds/KeybindProcesser.java
@@ -0,0 +1,10 @@
+package com.castiamc.client.keybinds;
+
+import com.castiamc.client.command.CmdProcessor;
+import com.castiamc.client.hack.HackList;
+
+public class KeybindProcessor {
+    public KeybindProcessor(HackList hax, KeybindList keybinds, CmdProcessor cmdProcessor) {
+        // placeholder constructor
+    }
+}
