10d45636bd81e725d3eaac5bc1476191f677e122
Hexon <snicks.phyla0y@icloud.com>
2025-12-24 18:44:03 -0500

    Create CCUpdater.java

---
+++ src/main/java/com/castiamc/client/update/CCUpdater.java
@@ -0,0 +1,17 @@
+package com.castiamc.client.update;
+
+import com.castiamc.client.events.UpdateListener;
+
+public class CCUpdater implements UpdateListener {
+
+    public CCUpdater() {
+        // Placeholder constructor
+        // You can add real update logic in the future
+    }
+
+    @Override
+    public void onUpdate() {
+        // Placeholder method
+        // For v1, no updating is done
+    }
+}
