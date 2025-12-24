fd1c0b0f7a522e0bcabeca3cf86c6324f7682ef1
Hexon <snicks.phyla0y@icloud.com>
2025-12-24 18:59:14 -0500

    Add JsonException class for JSON processing errors

---
+++ src/main/java/com/castiamc/client/util/json/JsonException.java
@@ -0,0 +1,6 @@
+package com.castiamc.client.util.json;
+
+public class JsonException extends Exception {
+    public JsonException() { super(); }
+    public JsonException(String msg) { super(msg); }
+}
