0d14d6a4ce2bb37339bbaa6d37e5644599fd75ec
Hexon <snicks.phyla0y@icloud.com>
2025-12-24 18:49:13 -0500

    Add PlausibleAnalytics class with placeholder methods

---
+++ src/main/java/com/castiamc/client/analytics/PlausibleAnalytics.java
@@ -0,0 +1,13 @@
+package com.castiamc.client.analytics;
+
+import java.nio.file.Path;
+
+public class PlausibleAnalytics {
+    public PlausibleAnalytics(Path file) {
+        // placeholder constructor
+    }
+
+    public void pageview(String page) {
+        // placeholder, does nothing
+    }
+}
