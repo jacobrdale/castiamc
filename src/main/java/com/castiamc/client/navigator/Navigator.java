a2e2d83c0978d91517350e8540d7ba028d379f52
Hexon <snicks.phyla0y@icloud.com>
2025-12-24 18:55:47 -0500

    Add Navigator class with constructor

---
+++ src/main/java/com/castiamc/client/navigator/Navigator.java
@@ -0,0 +1,13 @@
+package com.castiamc.client.navigator;
+
+import com.castiamc.client.command.CmdList;
+import com.castiamc.client.hack.HackList;
+import com.castiamc.client.other_feature.OtfList;
+
+import java.nio.file.Path;
+
+public class Navigator {
+    public Navigator(Path file, HackList hax, CmdList cmds, OtfList otfs) {
+        // placeholder
+    }
+}
